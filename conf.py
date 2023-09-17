e17655161301c7e5461bd75e98cbc1752ca8cb84
yamashun99 <yamashun.103099@gmail.com>
2023-09-17 17:22:00 +0900

    plot

--- conf.py
+++ conf.py
@@ -33,9 +33,22 @@ class Wolrdline:
         lam = 1 / 4 * J
         self.kinks = [[] for i in range(L)]
         self.kink = namedtuple("kink", ["tau", "spin"])
+        self.initial_spin = [-1 for i in range(L)]
 
     def add_kinks(self, x, tau, spin):
         self.kinks[x].append(self.kink(tau, spin))
+
+    def initial_spin_update(self, spin):
+        for x in range(self.L):
+            if len(self.kinks[x]) != 0 and len(self.kinks[x - 1]) == 0:
+                spin[x] = self.kinks[x][0].spin[0]
+            elif len(self.kinks[x]) == 0 and len(self.kinks[x]) != 0:
+                spin[x] = self.kinks[x][0].spin[0]
+            elif len(self.kinks[x]) != 0 and len(self.kinks[x]) != 0:
+                if self.kinks[x][0].tau < self.kinks[x - 1][0].tau:
+                    spin[x] = self.kinks[x][0].spin[0]
+                else:
+                    spin[x] = self.kinks[x - 1][0].spin[-1]
 
     def get_up(self, x):
         tau_spin = namedtuple("tau_spin", ["tau", "spin"])
@@ -52,15 +65,22 @@ class Wolrdline:
         up = []
         for i in range(len(tau)):
             if tau[i - 1].spin == 1:
-                up.append([tau[i - 1].tau, tau[i].tau])
+                up.append([tau[i - 1].tau, tau[i].tau, x])
         if up[0][0] > up[0][1]:
-            up.append([0, up[0][1]])
-            up.append([up[0][0], self.beta])
+            up.append([0, up[0][1], x])
+            up.append([up[0][0], self.beta, x])
             up.pop(0)
         return up
 
     def get_ups(
         self,
     ):
-        ups = [self.get_up(i) for i in range(self.L)]
+        ups = []
+        for x in range(self.L):
+            if len(self.kinks[x]) == len(self.kinks[x - 1]) == 0:
+                if self.initial_spin[x] == 1:
+                    ups += [[0, self.beta, x]]
+            else:
+                ups += self.get_up(x)
+        ups = np.array(ups)
         return ups
