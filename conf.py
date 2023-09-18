5b17f268554b000692a56c9f5d72a5187a43854e
yamashun99 <yamashun.103099@gmail.com>
2023-09-18 18:45:54 +0900

    cluster

--- conf.py
+++ conf.py
@@ -17,70 +17,82 @@ def gen_uniformly_events(beta, lam):
     return (tauk, n)
 
 
-class Kink:
+class Conf:
     def __init__(self, beta, J, L):
         self.beta = beta
         self.J = J
         self.L = L
-        lam = 1 / 4 * J
-
-
-class Wolrdline:
-    def __init__(self, beta, J, L):
-        self.beta = beta
-        self.J = J
-        self.L = L
-        lam = 1 / 4 * J
+        self.lam = 1 / 4 * J
+        self.kink = namedtuple("kink", ["ktau", "graph"])
         self.kinks = [[] for i in range(L)]
-        self.kink = namedtuple("kink", ["tau", "spin"])
-        self.initial_spin = [-1 for i in range(L)]
+        self.b = namedtuple("b", ["kx", "ktau", "iktau", "LR"])
+        # bs[bx][ibtau] = (kx, ktau, iktau, LR)
+        self.bs = [[] for i in range(L)]
+        # btilde = {(kx, iktau, LR): (bx, ibtau)}
+        self.btilde = {}
+        # c[x][itLR] = (bx, ibtau)
+        self.cluster = [[] for i in range(self.L)]
 
-    def add_kinks(self, x, tau, spin):
-        self.kinks[x].append(self.kink(tau, spin))
+    def root(self, x, t):
+        r = self.cluster[x][t]
+        while r != self.cluster[r[0]][r[1]]:
+            r = self.cluster[r[0]][r[1]]
+        return r
 
-    def initial_spin_update(self, spin):
-        for x in range(self.L):
-            if len(self.kinks[x]) != 0 and len(self.kinks[x - 1]) == 0:
-                spin[x] = self.kinks[x][0].spin[0]
-            elif len(self.kinks[x]) == 0 and len(self.kinks[x]) != 0:
-                spin[x] = self.kinks[x][0].spin[0]
-            elif len(self.kinks[x]) != 0 and len(self.kinks[x]) != 0:
-                if self.kinks[x][0].tau < self.kinks[x - 1][0].tau:
-                    spin[x] = self.kinks[x][0].spin[0]
-                else:
-                    spin[x] = self.kinks[x - 1][0].spin[-1]
-
-    def get_up(self, x):
-        tau_spin = namedtuple("tau_spin", ["tau", "spin"])
-        tau_right = []
-        for i in range(len(self.kinks[x])):
-            tau_right.append(tau_spin(self.kinks[x][i].tau, self.kinks[x][i].spin[0]))
-        tau_left = []
-        for i in range(len(self.kinks[x - 1])):
-            tau_left.append(
-                tau_spin(self.kinks[x - 1][i].tau, self.kinks[x - 1][i].spin[-1])
-            )
-        tau = tau_left + tau_right
-        tau.sort()
-        up = []
-        for i in range(len(tau)):
-            if tau[i - 1].spin == 1:
-                up.append([tau[i - 1].tau, tau[i].tau, x])
-        if up[0][0] > up[0][1]:
-            up.append([0, up[0][1], x])
-            up.append([up[0][0], self.beta, x])
-            up.pop(0)
-        return up
-
-    def get_ups(
-        self,
-    ):
-        ups = []
-        for x in range(self.L):
-            if len(self.kinks[x]) == len(self.kinks[x - 1]) == 0:
-                if self.initial_spin[x] == 1:
-                    ups += [[0, self.beta, x]]
+    def bond_cluster(self, x0, t0, x1, t1):
+        r0 = self.root(x0, t0)
+        r1 = self.root(x1, t1)
+        if r0[0] < r1[0]:
+            self.cluster[r1[0]][r1[1]] = r0
+        elif r0[0] > r1[0]:
+            self.cluster[r0[0]][r0[1]] = r1
+        else:
+            if r0[1] < r1[1]:
+                self.cluster[r1[0]][r1[1]] = r0
             else:
-                ups += self.get_up(x)
-        ups = np.array(ups)
-        return ups
+                self.cluster[r0[0]][r0[1]] = r1
+
+    def add_kinks(self, kx, ktau, graph):
+        self.kinks[kx].append(self.kink(ktau, graph))
+
+    def make_b(self):
+        for kx in range(self.L):
+            for iktau, kink in enumerate(self.kinks[kx]):
+                self.bs[kx].append(self.b(kx, kink.ktau, iktau, 1))
+            for itau, kink in enumerate(self.kinks[kx - 1]):
+                self.bs[kx].append(self.b((kx - 1) % self.L, kink.ktau, itau, -1))
+            self.bs[kx].sort(key=lambda b: b.ktau)
+
+    def make_btilde(self):
+        for bx in range(self.L):
+            for ibtau in range(len(self.bs[bx])):
+                self.btilde[
+                    (
+                        self.bs[bx][ibtau].kx,
+                        self.bs[bx][ibtau].iktau,
+                        self.bs[bx][ibtau].LR,
+                    )
+                ] = (bx, ibtau)
+
+    def initial_cluster(self):
+        for bx in range(self.L):
+            for ibtau in range(len(self.bs[bx])):
+                self.cluster[bx].append([bx, ibtau])
+
+    def cluster_update(self):
+        self.initial_cluster()
+        print(self.cluster)
+        for kx in range(self.L):
+            for iktau in range(len(self.kinks[kx])):
+                bx0, ibtau0 = self.btilde[(kx, iktau, 1)]
+                bx1, ibtau1 = self.btilde[(kx, iktau, -1)]
+                bx2 = bx0
+                ibtau2 = ibtau0 - 1
+                bx3 = bx1
+                ibtau3 = ibtau1 - 1
+                if self.kinks[kx][iktau].graph == "h":
+                    self.bond_cluster(bx0, ibtau0, bx1, ibtau1)
+                    self.bond_cluster(bx2, ibtau2, bx3, ibtau3)
+                elif self.kinks[kx][iktau].graph == "d":
+                    self.bond_cluster(bx0, ibtau0, bx3, ibtau3)
+                    self.bond_cluster(bx1, ibtau1, bx2, ibtau2)
