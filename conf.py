f4d79c54e4ad6d6c5de26dd39747d1cf2406afd6
yamashun99 <yamashun.103099@gmail.com>
2023-09-18 22:58:03 +0900

    spin

--- conf.py
+++ conf.py
@@ -30,8 +30,10 @@ class Conf:
         self.bs = [[] for i in range(L)]
         # btilde = {(kx, iktau, LR): (bx, ibtau)}
         self.btilde = {}
-        # c[x][itLR] = (bx, ibtau)
+        # c[bx][ibtau] = (bx, ibtau)
         self.cluster = [[] for i in range(self.L)]
+        # spin[bx][ibtau] = (bx, ibtau)
+        self.spin = [[[0, -1]] for i in range(self.L)]
 
     def root(self, x, t):
         r = self.cluster[x][t]
@@ -58,9 +60,9 @@ class Conf:
     def make_b(self):
         for kx in range(self.L):
             for iktau, kink in enumerate(self.kinks[kx]):
-                self.bs[kx].append(self.b(kx, kink.ktau, iktau, 1))
+                self.bs[kx].append(self.b(kx, kink.ktau, iktau, "L"))
             for itau, kink in enumerate(self.kinks[kx - 1]):
-                self.bs[kx].append(self.b((kx - 1) % self.L, kink.ktau, itau, -1))
+                self.bs[kx].append(self.b((kx - 1) % self.L, kink.ktau, itau, "R"))
             self.bs[kx].sort(key=lambda b: b.ktau)
 
     def make_btilde(self):
@@ -84,8 +86,8 @@ class Conf:
         print(self.cluster)
         for kx in range(self.L):
             for iktau in range(len(self.kinks[kx])):
-                bx0, ibtau0 = self.btilde[(kx, iktau, 1)]
-                bx1, ibtau1 = self.btilde[(kx, iktau, -1)]
+                bx0, ibtau0 = self.btilde[(kx, iktau, "L")]
+                bx1, ibtau1 = self.btilde[(kx, iktau, "R")]
                 bx2 = bx0
                 ibtau2 = ibtau0 - 1
                 bx3 = bx1
@@ -96,3 +98,44 @@ class Conf:
                 elif self.kinks[kx][iktau].graph == "d":
                     self.bond_cluster(bx0, ibtau0, bx3, ibtau3)
                     self.bond_cluster(bx1, ibtau1, bx2, ibtau2)
+
+    def inert_spin(self, kinks):
+        for kx in range(self.L):
+            for iktau in range(len(kinks[kx])):
+                bx0, ibtau0 = self.btilde[(kx, iktau, "L")]
+                bx1, ibtau1 = self.btilde[(kx, iktau, "R")]
+                tau = kinks[kx][iktau].ktau
+                self.spin[bx0].append([tau, None])
+                self.spin[bx1].append([tau, None])
+        for bx in range(self.L):
+            self.spin[bx].sort(key=lambda s: s[0])
+            for ibtau in range(len(self.spin[bx])):
+                if self.spin[bx][ibtau][1] == None:
+                    self.spin[bx][ibtau][1] = self.spin[bx][ibtau - 1][1]
+            print(self.spin[bx])
+            print(len(self.spin[bx]))
+            if len(self.spin[bx]) > 1 and self.spin[bx][0][0] == 0:
+                self.spin[bx].pop(0)
+
+    def spin_update(self):
+        flip = [[] for i in range(self.L)]
+        for kx in range(self.L):
+            for itau in range(len(self.spin[kx])):
+                flip[kx].append(random.choice([-1, 1]))
+        for bx in range(self.L):
+            for ibtau in range(len(self.spin[bx])):
+                if len(self.cluster[bx]) > 1:
+                    rbx, rbtau = self.root(bx, ibtau)
+                else:
+                    rbx, rbtau = bx, ibtau
+                self.spin[bx][ibtau][1] *= flip[rbx][rbtau]
+
+    def conf_update(self):
+        for kx in range(self.L):
+            for iktau in range(len(self.kinks[kx])):
+                print(self.kinks[kx][iktau].graph)
+                self.kinks[kx][iktau].graph = random.choice(["h", "d"])
+        for kx in range(self.L):
+            tauk, n = gen_uniformly_events(self.beta, self.lam)
+            for tau in tauk:
+                self.add_kinks(kx, tau, "x")
